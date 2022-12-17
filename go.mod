1b09e64dfd65c3d1a49fe4f8b123d403774c6629
Luca Sepe <luca.sepe@gmail.com>
2022-12-17 16:15:03 +0100

    add slug function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lucasepe/toolbox
 
 go 1.19
+
+require golang.org/x/text v0.5.0
---
+++ slug/slug_test.go
@@ -0,0 +1,31 @@
+package slug
+
+import (
+	"testing"
+)
+
+var tests = []struct{ in, out string }{
+	{"simple test", "simple-test"},
+	{"I'm go developer", "i-m-go-developer"},
+	{"Simples código em go", "simples-codigo-em-go"},
+	{"日本語の手紙をテスト", "日本語の手紙をテスト"},
+	{"--->simple test<---", "simple-test"},
+}
+
+func TestSlugify(t *testing.T) {
+	for _, test := range tests {
+		if out := Slugify(test.in); out != test.out {
+			t.Errorf("%q: %q != %q", test.in, out, test.out)
+		}
+	}
+}
+
+func TestSlugifyf(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.out, func(t *testing.T) {
+			if out := Slugifyf("%s", test.in); out != test.out {
+				t.Errorf("%q: %q != %q", test.in, out, test.out)
+			}
+		})
+	}
+}
